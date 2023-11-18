a89eb689baaf933ce9a17ddeabaa96141d1a0577
Zazzaro <sassaro01cristian@gmail.com>
2023-11-18 14:59:03 +0100

    did bonus and make the page prettier

--- main.js
+++ main.js
@@ -5,25 +5,60 @@ const toDoList = {
             newTask: "",
 
             toDoTasks: [
-                'Buy bread',
-                'Buy eggs',
-                'Buy cookies',
-                'Go jogging',
-                'Take a shower',
-                'Make lunch',
-                'Clean the house',
-                'Wash car'
+                {
+                    text: 'Buy bread',
+                    done: false
+                },
+                {
+                    text: 'Buy eggs',
+                    done: false
+                },
+                {
+                    text: 'Buy cookies',
+                    done: false
+                },
+                {
+                    text: 'Go jogging',
+                    done: false
+                },
+                {
+                    text: 'Take a shower',
+                    done: false
+                },
+                {
+                    text: 'Make lunch',
+                    done: false
+                },
+                {
+                    text: 'Clean the house',
+                    done: false
+                },
+                {
+                    text: 'Wash car',
+                    done: false
+                },
             ],
         };
     },
     methods: {
         addTask() {
-            if (this.newTask == "") {
-                alert("Please enter task");
+            if (this.newTask === "") {
+                alert("Please enter a task");
             } else {
-                this.toDoTasks.push( this.newTask );
+                this.toDoTasks.push({
+                    text: this.newTask,
+                    done: false
+                });
                 this.newTask = "";
             }
+        },
+
+        markAsComplete() {
+         this.toDoTasks.filter(task => task.done);
+        },
+
+        deleteTask(index) {
+            this.toDoTasks.splice(index, 1);
         }
 
         
